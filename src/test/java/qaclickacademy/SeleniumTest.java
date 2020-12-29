775a273ff956e1e7a3440cb8c0193d7c1f461eec
Sanket Vaviya <djsann.78693@gmail.com>
2020-12-29 13:24:45 +0530

    firstcommit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	//TestNG sml from maven
+	 
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void ElementsUI()
+	{
+		System.out.println("ElementsUI");
+	}	
+}
